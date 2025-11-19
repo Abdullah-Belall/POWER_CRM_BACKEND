8210d4694ae795b899fc026b90764541ad8031e8
Abdullah <abdullahbelal102938@gmail.com>
2025-11-19 11:22:44 +0200

    tany

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -272,7 +272,6 @@ export class AuthService {
       sameSite: 'none',
       expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
       priority: 'high',
-      domain: '.vercel.app',
     });
     return {
       done: true,
